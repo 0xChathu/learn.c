94c54759fde0b4cd1a2dcd6ebef27738f6dc1518
0xChathu <shcpraveen@outlook.com>
2025-10-11 23:37:17 +0530

    Update Cake_Shop.c
    Hey ... This small cakeshop took me hours to build.
    Ha ha ... I enjoyed tho, got help from DeepSeek tho.

--- Projects/Cake_Shop.c
+++ Projects/Cake_Shop.c
@@ -2,9 +2,10 @@
 int main (void)
 
 {
-int order;
+int order, quantity;
 char more;
 float total = 0.0;
+float price;
 
 puts("|• WELCOME TO CHATHU CAKE SHOP •|");
 puts("");
@@ -16,39 +17,45 @@ do {
 puts("¥¥-- CAKE MENU --¥¥");
 puts("#1. Butter Cake");
 puts("#2 Chocolate Cake");
-puts("#3 Carromal Cake");
+puts("#3 Caramel Cake");
 puts("#4 Icing Cake");
 puts("#5 Velvet Cake");
 puts("");
 printf("What cake you wish to order (1-5) : ");
 scanf("%d", &order);
+printf("Quantity : \n");
+scanf("%d",&quantity);
 
     switch (order)
-    {
+                {
     case 1:
-    printf("∆ Butter Cake ∆ successfully ordered | Price would be Rs.500 \n");
-    total +=500;
+    price = 350; printf("~Butter Cake");
     break;
+    
     case 2:
-    printf("∆ chocolate Cake ∆ successfully ordered | Price would be Rs.650 \n");
-    total +=650;
+    price = 250; printf("~Chocolate Cake");
     break;
+    
     case 3:
-    printf("∆ Carromal Cake ∆ successfully ordered | Price would be Rs.450 \n");
-    total +=450;
+    price = 150; printf("~Caramel Cake");
     break;
+    
     case 4:
-    printf("∆ icing Cake ∆ successfully ordered | Price would be Rs.1500 \n");
-    total +=1500;
+    price = 1500; printf("~icing Cake");
     break;
+    
     case 5:
-    printf("∆ Velvet Cake ∆ successfully ordered | Price would be Rs.6500 \n");
-    total +=6500;
+    price = 2000; printf("~velvet Cake");
     break;
+    
     default:
     printf("Invalid Choice, Please select 1-5\n");
     continue;
-    }
+                }
+    
+    float ItemTotal = price * quantity;
+    total += ItemTotal;
+    printf("x%d added! +Rs.%.2f\n", quantity, ItemTotal);
     
     printf("\nAnything else ? : (Y/N) ");
     scanf(" %c", &more);
